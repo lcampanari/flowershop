74df38ea73abb8faa09858ddd1869bf834223123
Lucas Campanari <campanari.ls@gmail.com>
2020-02-13 13:54:17 -0500

    feat(api): api endpoint for listing products

---
+++ backend/app/Controllers/Http/ProductController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Product = use('App/Models/Product')
+const Helpers = use('Helpers')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with products
+ */
+class ProductController {
+  /**
+   * Show a list of all products.
+   * GET products
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index () {
+    const products = await Product.all()
+    return products
+  }
+}
+
+module.exports = ProductController
