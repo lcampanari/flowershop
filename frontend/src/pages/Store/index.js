4f83d2495ad8fee19de86f15223afd8f2cd1d1b6
Lucas Campanari <campanari.ls@gmail.com>
2020-02-13 15:34:07 -0500

    feat(app): list products

--- frontend/src/pages/Store/index.js
+++ frontend/src/pages/Store/index.js
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
 
 export default function Store () {
-  return <div>Store</div>
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    async function loadProducts () {
+      const res = await api.get('/products')
+      setProducts(res.data)
+    }
+
+    loadProducts()
+  }, [])
+
+  return (
+    <>
+      <div className='products-list'>
+        {products.map(product => (
+          <div className='product-card' key={product.id}>
+            <div>
+              {product.name}: ${product.price}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
