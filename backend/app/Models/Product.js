20dfc0bdef904397cf343f587257da4d4bf8f09f
Lucas Campanari <campanari.ls@gmail.com>
2020-02-13 19:25:20 -0500

    feat: add thumbnail url to list of products

--- backend/app/Models/Product.js
+++ backend/app/Models/Product.js
@@ -2,8 +2,17 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Env = use('Env')
 
 class Product extends Model {
+  static get computed () {
+    return ['thumbnail_url']
+  }
+
+  getThumbnailUrl ({ image }) {
+    const url = Env.get('APP_URL')
+    return `${url}/uploads/product/${image}`
+  }
 }
 
 module.exports = Product
