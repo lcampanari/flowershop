0394d526ba29add09d0ff83e1ce604ba371979af
Lucas Campanari <campanari.ls@gmail.com>
2020-02-14 19:54:00 -0500

    feat(rating): display rating with support for decimal rating

--- frontend/src/components/Rating/index.js
+++ frontend/src/components/Rating/index.js
@@ -1,19 +1,64 @@
+import classNames from 'classnames'
 import React from 'react'
-import { ReactComponent as Star } from '../../assets/star.svg'
+import { ReactComponent as StarOriginal } from '../../assets/star.svg'
 import './styles.scss'
+
+function Star () {
+  return <StarOriginal fill='currentColor' />
+}
 
 export default function Rating ({ rating, max = 5 }) {
   const stars = []
+  const starFull = Math.floor(rating)
+  const starPart = rating - starFull
 
   for (let i = 1; i <= max; i++) {
-    stars.push(<Star />)
+    let star
+    // full stars
+    if (i <= starFull) {
+      star = {
+        type: 'full',
+        elem: <Star />
+      }
+      // partial stars
+    } else if (starPart && i > starFull + starPart && i <= starFull + 1) {
+      star = {
+        type: 'partial',
+        elem: (
+          <>
+            <div
+              className='rating__partial'
+              style={{ width: `${starPart * 100}%` }}
+            >
+              <Star />
+            </div>
+            <Star />
+          </>
+        )
+      }
+      // grey stars
+    } else {
+      star = {
+        type: 'empty',
+        elem: <Star />
+      }
+    }
+    stars.push(star)
   }
 
   return (
     <div className='rating'>
       {stars.map((star, i) => (
-        <div className='rating__point' key={i}>
-          {star}
+        <div
+          className={classNames('rating__star', {
+            'rating__star--threshold':
+              star.type === 'full' &&
+              stars[i + 1] &&
+              stars[i + 1].type !== 'full'
+          })}
+          key={i}
+        >
+          {star.elem}
         </div>
       ))}
     </div>
